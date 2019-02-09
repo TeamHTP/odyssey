6fb2f7339dab1ef57ec8cf112c8b5ca3862402a1
Tianyi Ma <tn.ma@outlook.com>
2019-02-09 00:08:40 -0600

    api

--- src/main/java/com/teamhtp/odyssey/logic/Attractions.java
+++ src/main/java/com/teamhtp/odyssey/logic/Attractions.java
@@ -10,15 +10,16 @@ import spark.Route;
 public class Attractions {
 
     public static Route getAtLocation = (q, a) -> {
-        long rad = Long.getLong(q.queryParams("rad"));
-        long lat = Long.getLong(q.queryParams("lat"));
-        long lng = Long.getLong(q.queryParams("lng"));
+        String rad = q.queryParams("rad");
+        String lat = q.queryParams("lat");
+        String lng = q.queryParams("lng");
         String sessionId = q.session().attribute(SessionAttribute.USER_SESSION_ID);
-        HttpResponse<String> jsonResponse = Unirest.get("https://maps.googleapis.com/maps/api/place/findplacefromtext/json")
+        HttpResponse<String> jsonResponse = Unirest.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json")
                 .header("accept", "application/json")
                 .queryString("key", Main.GOOGLE_MAPS_API_KEY)
-                .queryString("inputtype", "textquery")
-                .queryString("locationbias", "circle:" + rad + "@" + lat + "," + lng)
+                .queryString("radius", rad)
+                .queryString("location", lat + "," + lng)
+                .queryString("keyword", "attractions")
                 .asString();
         return new Gson().toJson(jsonResponse.getBody());
     };
